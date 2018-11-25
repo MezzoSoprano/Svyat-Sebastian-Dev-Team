c37d8010129ab57c8f04496c111fdd4ea0ed7c5c
mezzo_soprano <svyatkokatola@gmail.com>
2018-11-25 03:04:14 +0200

    TableViewCell interface working [HW 6]

--- Diary/Diary/ViewController.swift
+++ Diary/Diary/ViewController.swift
@@ -15,23 +15,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var tableView = UITableView()
     var tableViewConstraints: [NSLayoutConstraint] = []
+    let tableCellID = "CellID"
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.backgroundColor = UIColor.white
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "my")
-        
-        self.view.addSubview(tableView)
-        
-        //constaints for tableview
-        setupTableViewLayout()
+        //tableview
+        setupTableView()
     }
     
-    private func setupTableViewLayout() {
+    private func setupTableView() {
+        self.view.addSubview(tableView)
+        tableView.register(tableCell.self, forCellReuseIdentifier: tableCellID)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        tableView.backgroundColor = UIColor(r: 128, g: 126, b: 124)
+        //constarints adding
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
@@ -44,14 +46,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "my", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row].simpleDescription()
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -64,6 +60,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             detailVC.recivedItemIndex = itemTemp
     }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath)
     {
         
@@ -74,15 +75,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.performSegue(withIdentifier: "segueForPassingData", sender: nil)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        tableView.reloadData()
-    }
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            print("Deleted")
             list.remove(at: indexPath.row)
             self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            print("Note was deleted: \(list[indexPath.row].name ?? "default value")")
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableCellID, for: indexPath) as! tableCell
+        cell.textLabel?.text = list[indexPath.row].simpleDescription()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+}
+
+class tableCell: UITableViewCell {
+    let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 184, g: 176, b: 170)
+        view.setCellShadow()
+        return view
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+
+    func setup() {
+        addSubview(cellView)
+        self.selectionStyle = UITableViewCell.SelectionStyle.none
+//        self.setHighlighted(true, animated: true)
+        self.backgroundColor = UIColor(r: 128, g: 126, b: 124)
+        cellView.setAnchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 8, paddingBottom: 4, paddingRight: 8)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
