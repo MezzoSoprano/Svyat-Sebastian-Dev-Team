742de5f76e697be29ecd1c97eeaee0bdd3c4578f
sebalaursen <sebalaursen@gmail.com>
2018-12-03 00:52:54 +0200

    Added switch from tableView to collectionView

--- Diary/Diary/SettingsViewController.swift
+++ Diary/Diary/SettingsViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-     @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var viewSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,7 @@ class SettingsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         themeSwitch.isOn = (Settings.currentTheme.accent == .white) ? true : false //govnogod
+        viewSwitch.isOn = (Settings.isCollectionView == false) ? false : true
     }
     
     @IBAction func themeChanged(_ sender: UISwitch) {
@@ -32,8 +34,15 @@ class SettingsViewController: UIViewController {
         view.backgroundColor = Settings.currentTheme.background
         self.navigationController?.navigationBar.barTintColor = Settings.currentTheme.background
     }
+    
+    @IBAction func viewChanger(_ sender: UISwitch) {
+        Settings.isCollectionView = sender.isOn
+        
+        UserDefaults.standard.set(sender.isOn, forKey: "CollectionView")
+    }
 }
 
 class Settings {
     static var currentTheme: ThemeProtocol = LightTheme()
+    static var isCollectionView = false
 }
